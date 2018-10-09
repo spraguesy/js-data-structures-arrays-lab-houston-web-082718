0646ab3c0b79b5c038e260a9850e4518500fb834
Flatiron School <andrewsprague@outlook.com>
2018-10-08 20:43:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift(name);
+}
+
+function appendDriver(name) {
+    let new_array = drivers.slice();
+    new_array.push(name);
+    return new_array
+};
+
+function prependDriver(name) {
+    let new_array = drivers.slice();
+    new_array.unshift(name);
+    return new_array;
+}
+
+function removeLastDriver(name) {
+    let new_array = drivers.slice();
+    new_array.pop(name);
+    return new_array;
+};
+
+function removeFirstDriver(name) {
+    let new_array = drivers.slice();
+    new_array.shift(name);
+    return new_array;
+};
+
+
+
+
+
